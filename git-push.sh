726ecc0919e7a8e63142fd3556a406c7124b3e2c
masterrjj24 <masterjj24@gmail.com>
2020-07-31 11:57:04 -0500

    better

--- git-push.sh
+++ git-push.sh
@@ -1,6 +1,6 @@
 # Este es un comentario: Script automatico para subir los cambios rapidamente
 git config --global user.email "masterjj24@gmail.com"
-git config --global user.name "Jahir Higuera"
+git config --global user.name "masterrjj24"
 
 git add .
 git commit -am "better"
